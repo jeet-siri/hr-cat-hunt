b49775204f33366a1a0c79774869a2efce671244
Jeet Sirikulthada <jeet.sisb@gmail.com>
2026-09-10 09:58:29 +0700

    Fix database URL scheme for PostgreSQL on Render

--- database.py
+++ database.py
@@ -8,6 +8,10 @@ SQLALCHEMY_DATABASE_URL = os.environ.get(
     "DATABASE_URL", "sqlite:///./test.db"
 )
 
+# Render / Heroku postgres URLs start with 'postgres://', which SQLAlchemy 2.0 requires to be 'postgresql://'
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
     engine = create_engine(
         SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
